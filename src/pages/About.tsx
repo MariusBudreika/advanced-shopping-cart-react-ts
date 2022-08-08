5135ce1ee08fd94d769bd04fc5bb0bc1fbeaac59
Marius <marius.budreika09@gmail.com>
2022-08-08 13:38:44 +0300

    fixed margin

--- src/pages/About.tsx
+++ src/pages/About.tsx
@@ -1,7 +1,12 @@
 import React from "react";
+import MainLayout from "../components/MainLayout/MainLayout";
 
 const About = () => {
-  return <h1>About</h1>;
+  return (
+    <MainLayout>
+      <h1>About</h1>
+    </MainLayout>
+  );
 };
 
 export default About;
