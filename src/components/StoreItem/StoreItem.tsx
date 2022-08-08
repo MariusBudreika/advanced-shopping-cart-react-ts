9762b39081af9e4f914449ceb48668a5a0379a04
Marius <marius.budreika09@gmail.com>
2022-08-08 16:22:09 +0300

    updated store item, added util to format currency

--- src/components/StoreItem/StoreItem.tsx
+++ src/components/StoreItem/StoreItem.tsx
@@ -1,4 +1,5 @@
 import React from "react";
+import { formatCurrency } from "../../utils/formatCurrency";
 import styles from "./StoreItem.module.scss";
 
 type StoreItemProps = {
@@ -9,12 +10,29 @@ type StoreItemProps = {
 };
 
 const StoreItem = ({ id, name, price, imgUrl }: StoreItemProps) => {
+  const quantity = 2;
   return (
     <div className={styles.card}>
       <img src={imgUrl} className={styles.cardImages} alt="" />
       <div className={styles.info}>
-        <div className={styles.infoLabel}>{name}</div>
-        <div className={styles.infoPrice}>${price}</div>
+        <span className={styles.infoLabel}>{name}</span>
+        <span className={styles.infoPrice}>{formatCurrency(price)}</span>
+      </div>
+      <div className={styles.cardBottom}>
+        {quantity < 1 ? (
+          <button className={styles.addToCart}>+ Add To Cart</button>
+        ) : (
+          <div className={styles.cart}>
+            <div className={styles.cartTop}>
+              <button className={styles.cartTopBtn}>-</button>
+              <div className={styles.cartTopQuantity}>{quantity} in cart</div>
+              <button className={styles.cartTopBtn}>+</button>
+            </div>
+            <div className={styles.cartBottom}>
+              <button className={styles.cartBottomRemove}>Remove</button>
+            </div>
+          </div>
+        )}
       </div>
     </div>
   );
